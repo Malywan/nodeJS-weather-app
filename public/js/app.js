49da28fcda7d444f05cbddf3b2f7035d180dc4e1
Malywan <dragondiveu@gmail.com>
2019-08-31 13:50:14 +0100

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,8 @@ weatherForm.addEventListener('submit', (e) => {
     forecast.className='flow-text'
     forecast.textContent = 'Loading forecast ...'
     //console.log(locationField.value)
-    fetch(`http://localhost:3000/weather?address=${locationField.value}`).then((response) => {
+    // We ommit the https://localhost:3000 to make the port flexible and deploy the app online
+    fetch(`/weather?address=${locationField.value}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 forecast.className='flow-text red-text'
